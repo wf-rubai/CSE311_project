1ca80042bbfe57d06a117df6ff5b80c02b5089d5
wf-rubai <rubai4483302@gmail.com>
2024-10-25 23:42:47 +0600

    sql added and navbar, sidebar designed

--- php/MySQL.php
+++ php/MySQL.php
@@ -12,17 +12,5 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM users";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "Name: " . $row["name"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
 $conn->close();
 ?>
--- sql/courses.sql
+++ sql/courses.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 25, 2024 at 07:22 PM
+-- Generation Time: Oct 25, 2024 at 07:38 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
