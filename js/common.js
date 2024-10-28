c586df6d23c2a1ba6d11b0a066d866b06c3f55d8
wf-rubai <rubai4483302@gmail.com>
2024-10-28 21:40:24 +0600

    course add option done

--- js/common.js
+++ js/common.js
@@ -2,6 +2,5 @@ window.addEventListener('message', function(event) {
     if (event.data === 'toggleSidebar') {
         const sidebar = document.getElementById('sidebar');
         sidebar.style.display = sidebar.style.display === 'none' ? 'block' : 'none';
-        // Optional: sidebar.style.transition = '5s';
     }
 });
