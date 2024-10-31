afa26397c3a394e55f08ee7ecf89ed302ecc700a
wf-rubai <rubai4483302@gmail.com>
2024-10-31 14:00:49 +0600

    for pull

--- html/faculty_review.php
+++ html/faculty_review.php
@@ -318,7 +318,7 @@
                         <label for="reviewText">Review:</label>
                         <!-- <textarea id="reviewText" rows="4"></textarea> -->
                         <?php
-                        echo readfile("WFR_editor.html");
+                        echo readfile("template/WFR_editor.html");
                         ?>
 
                         <label>Learning:</label>
