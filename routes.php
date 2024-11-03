b33ed9844e84f6bba1c6cbe16760cd1c51c5a122
Arman Mokammel <armanmokammel@gmail.com>
2024-11-04 00:08:36 +0600

    Added dynamic url routing system

---
+++ routes.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    '/' => 'html/index.html',
+    '/template/side_bar.html' => 'html/template/side_bar.html',
+    '/template/nav_bar.html' => 'html/template/nav_bar.html',
+    '/login' => 'html/login.html',
+    '/signup' => 'html/signup.html',
+    '/profile' => 'html/profile.html',
+    '/routine' => 'html/routine.html',
+    '/faculty_review' => 'html/faculty_review.php',
+];
+
+?>
