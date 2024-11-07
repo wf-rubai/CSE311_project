68f6bc54d3c1a06b58b7a12ab9347eee3f386506
OYSHINABILA <oyshinabila261006@gmail.com>
2024-11-07 17:42:36 +0600

    rubai XX

--- routes.php
+++ routes.php
@@ -9,6 +9,7 @@ return [
     '/profile' => 'html/profile.html',
     '/routine' => 'html/routine.html',
     '/faculty_review' => 'html/faculty_review.php',
+    '/update_signup'=>'html/update_signup.html',
 ];
 
 ?>
