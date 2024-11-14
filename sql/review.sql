c4b90a0301712d873f6e2b8b5fc7820d3e4f124f
Arman Mokammel <armanmokammel@gmail.com>
2024-11-14 17:01:46 +0600

    .

--- sql/review.sql
+++ sql/review.sql
@@ -6,6 +6,7 @@ CREATE TABLE review (
     user_comment TEXT,
     review_by VARCHAR(255),
     is_anonymous BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     
     FOREIGN KEY (review_of) REFERENCES faculty(initial)   
 );
